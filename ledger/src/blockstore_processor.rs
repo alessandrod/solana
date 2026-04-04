e1160c002ed8c89fda836b98e25dde368bff8aad
Alessandro Decina <alessandro.d@gmail.com>
2026-04-05 02:23:01 +1000

    trace: shreds

--- ledger/src/blockstore_processor.rs
+++ ledger/src/blockstore_processor.rs
@@ -4468,6 +4468,7 @@ pub mod tests {
             None,
             None,
             &MigrationStatus::default(),
+            None,
         )
         .unwrap();
         let bank0_last_blockhash = bank0.last_blockhash();
@@ -4485,6 +4486,7 @@ pub mod tests {
             None,
             &mut ExecuteTimings::default(),
             &MigrationStatus::default(),
+            None,
         )
         .unwrap();
         bank_forks.write().unwrap().set_root(1, None, None);
